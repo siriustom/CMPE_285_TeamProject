9b2b0357b9a4efdac0c733064cd013f63927ce49
Mokuten OH <mokuteno@gmail.com>
2017-12-17 13:25:56 -0800

    expose error for convenience

--- app/views.py
+++ app/views.py
@@ -8,36 +8,36 @@ import json
 @app.route('/invest', methods=['GET', 'POST'])
 def invest():
     form = InvestForm(request.form)
+    # try:
+    #
+    # except:
+    #     return render_template('error.html')
     if request.method =='POST':
-        try:
-            form = InvestForm(request.form)
-            amount = float(request.form['amount'])
-            print(1)
-            if amount < 5000:
-                print('fucccccccccccccc')
-                return render_template('error.html')
-            print(2)
-            print(amount)
-            choices = request.form.getlist('strategies')
-            print(3)
-            if len(choices)<=0 or len(choices)>2:
-                return render_template('error.html')
-            print(choices)
-            print(4)
-            testArray = ['Ethical']
-            stocklist = get_stock_list_all(testArray)
-            details = get_strategy_stock_info(stocklist, amount)
-            print(6)
-            print(stocklist)
-            print('1234')
-            history = get_historical_strategy_stock_value(stocklist, amount)
-            print(7)
-            print(history)
-            return render_template("result.html", details=details, history=history)
-            #return render_template("result.html", details=details, data=map(json.dumps, details))
-
-        except:
+        form = InvestForm(request.form)
+        amount = float(request.form['amount'])
+        print(1)
+        if amount < 5000:
+            print('fucccccccccccccc')
             return render_template('error.html')
+        print(2)
+        print(amount)
+        choices = request.form.getlist('strategies')
+        print(3)
+        if len(choices) <= 0 or len(choices) > 2:
+            return render_template('error.html')
+        print(choices)
+        print(4)
+        testArray = ['Ethical']
+        stocklist = get_stock_list_all(testArray)
+        details = get_strategy_stock_info(stocklist, amount)
+        print(6)
+        print(stocklist)
+        print('1234')
+        history = get_historical_strategy_stock_value(stocklist, amount)
+        print(7)
+        print(history)
+        return render_template("result.html", details=details, history=history)
+        # return render_template("result.html", details=details, data=map(json.dumps, details))
 
     return render_template('invest.html', form=form,
                            strategy=app.config['STRATEGY'])
